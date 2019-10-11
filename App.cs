9284ff2005f249d0a3a9c04c96563008de3677de
Saša Barišić <sasa.barisic@serengetitech.com>
2019-10-11 14:09:16 +0200

    Latest

--- App.cs
+++ App.cs
@@ -1,24 +1,20 @@
 ﻿using Bridge;
+using Bridge.Html5;
 using Newtonsoft.Json;
 using System;
 
 namespace CarpWeb {
 	public class App {
 		public static void Main() {
-			// Write a message to the Console
-			Console.WriteLine("Welcome to Bridge.NET");
+			HTMLDivElement FileBrowser = Document.GetElementById<HTMLDivElement>("file_browser");
+			if (FileBrowser != null)
+				MainFileBrowser(FileBrowser);
 
-			// After building (Ctrl + Shift + B) this project, 
-			// browse to the /bin/Debug or /bin/Release folder.
 
-			// A new bridge/ folder has been created and
-			// contains your projects JavaScript files. 
+		}
 
-			// Open the bridge/index.html file in a browser by
-			// Right-Click > Open With..., then choose a
-			// web browser from the list
-
-			// This application will then run in the browser.
+		static void MainFileBrowser(HTMLDivElement FileBrowser) {
+			Console.WriteLine("This is a file browser");
 		}
 	}
 }
